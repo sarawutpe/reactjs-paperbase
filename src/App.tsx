1d6f4a62c0964d498ca79ec06dd9261e6e6edab1
Sarawut Pe <mr.sarawutpe@gmail.com>
2022-07-29 15:34:41 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import Paperbase from "./components/layouts";
 
-function App() {
+interface Props {}
+
+const App: React.FC<Props> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Paperbase>
+        <Paper sx={{ maxWidth: 936, margin: "auto", overflow: "hidden" }}>
+          <AppBar
+            position="static"
+            color="default"
+            elevation={0}
+            sx={{ borderBottom: "1px solid rgba(0, 0, 0, 0.12)" }}
+          >
+            <Toolbar>
+              <Grid container spacing={2} alignItems="center">
+                <Grid item>
+                  <SearchIcon color="inherit" sx={{ display: "block" }} />
+                </Grid>
+                <Grid item xs>
+                  <TextField
+                    fullWidth
+                    placeholder="Search by email address, phone number, or user UID"
+                    InputProps={{
+                      disableUnderline: true,
+                      sx: { fontSize: "default" },
+                    }}
+                    variant="standard"
+                  />
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" sx={{ mr: 1 }}>
+                    Add user
+                  </Button>
+                  <Tooltip title="Reload">
+                    <IconButton>
+                      <RefreshIcon color="inherit" sx={{ display: "block" }} />
+                    </IconButton>
+                  </Tooltip>
+                </Grid>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+          <Typography
+            sx={{ my: 5, mx: 2 }}
+            color="text.secondary"
+            align="center"
+          >
+            No users for this project yet
+          </Typography>
+        </Paper>
+      </Paperbase>
+    </>
   );
-}
+};
 
 export default App;
